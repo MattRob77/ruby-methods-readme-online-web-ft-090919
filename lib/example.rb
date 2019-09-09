67a5a6e2791a38f97e0d304015f3dd3922322bed
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-09 14:23:27 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -51,3 +51,22 @@ def say_hello_world_ten_times
   puts phrase
 end
 
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+ 
+say_hello_world_ten_times
+ 
+# ... The rest of the program
+ 
+say_hello_world_ten_times
